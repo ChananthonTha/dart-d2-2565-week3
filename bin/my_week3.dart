1d25750caf4c1cd7ba5dee079dc63f9bb9e8b749
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-06-02 16:10:12 +0700

    up

---
+++ bin/my_week3.dart
@@ -0,0 +1,10 @@
+void main(){
+  List<String> province = ['trat','Chantaburi','Rayong'];
+  print(province[0]);
+  print(province[1]);
+  print(province[2]);
+  print("==========");
+  for(var s in province){
+    print(s);
+  }
+}
